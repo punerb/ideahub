2831d5c2006512c23e00425f41e8700feff57d2d
Anuja <anuja@joshsoftware.com>
2012-04-06 19:30:48 +0530

    Initialize omniauth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+ TWITTER_KEY= '87rSkJ7tzINtNOefJ6FfGQ'
+ TWITTER_SECRET= 'TfXgJgKUo35olGwHNAY36HcMx2HhlNUHMSiNCXXjo'
+Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :developer unless Rails.env.production?
+      provider :twitter, TWITTER_KEY, TWITTER_SECRET
+end
