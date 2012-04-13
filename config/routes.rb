a2f8ac2552f1a1eda038cf4b232be9642190f095
Sandip Ransing <sandip@joshsoftware.com>
2012-04-13 19:28:54 +0530

    Twitter Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,30 @@ Ideahub::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
-  match '/auth/:provider/callback', to: 'sessions#create'
+# Omniauth pure
+  match "/signin" => "services#signin"
+  match "/signout" => "services#signout"
 
+  match '/auth/:service/callback' => 'services#create' 
+  match '/auth/failure' => 'services#failure'
+
+  resources :services, :only => [:index, :create, :destroy] do
+    collection do
+      get 'signin'
+      get 'signout'
+      get 'signup'
+      post 'newaccount'
+      get 'failure'
+    end
+  end
+
+  # used for the demo application only
+  resources :users, :only => [:index] do
+    collection do
+      get 'test'
+    end
+  end
+   
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
