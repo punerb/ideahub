eefc4d245254bd53f20c9083d8c8f7bcaeb035fb
Gautam Rege <gautam@joshsoftware.com>
2012-04-13 19:41:55 +0530

    Fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,13 @@
 Ideahub::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-# Omniauth pure
+  # Omniauth 
   match "/signin" => "services#signin"
   match "/signout" => "services#signout"
 
-  resources :ideas
   match '/auth/:service/callback' => 'services#create' 
   match '/auth/failure' => 'services#failure'
 
+  resources :ideas
   resources :services, :only => [:index, :create, :destroy] do
     collection do
       get 'signin'
@@ -35,50 +25,5 @@ Ideahub::Application.routes.draw do
     end
   end
    
->>>>>>> 456f06e69ab2f464563a5b273036260169b20ec7
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
   root :to => 'designs#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
