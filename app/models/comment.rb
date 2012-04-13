579eb8df3f73aeb95c8a0e790d5fe771e59fef22
swapnil <swapnil@joshsoftware.com>
2012-04-13 19:11:50 +0530

    Updated models and relationships. tested with data

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :idea
   belongs_to :user
   validates :description, :user_id, :idea_id, :presence => true
+
+  attr_accessible :description, :user_id, :idea_id
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,10 +1,12 @@
 class Idea < ActiveRecord::Base
 
-  has_many :users
-  has_and_belongs_to_many :categories
+  has_many :idea_users
+  has_many :users, :through => :idea_users
   has_many :comments, :dependent => :destroy
   has_many :schedules, :dependent => :destroy
+  has_many :idea_categories
+  has_many :categories, :through => :idea_categories
 
   validates :user_id, :title, :description, :presence => true
-
+  attr_accessible :title, :description, :user_id
 end
