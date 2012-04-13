fe46fbf64b7677c27178e165adf74bf39c29b996
sivagollapalli <siva@joshsoftware.com>
2012-04-13 20:08:28 +0530

    Updated Gemfile
    - Added implementation for specs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120407142824) do
+ActiveRecord::Schema.define(:version => 20120413132703) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(:version => 20120407142824) do
     t.text     "description"
     t.integer  "user_id"
     t.integer  "idea_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "idea_categories", :force => true do |t|
+    t.integer  "idea_id"
+    t.integer  "category_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
   end
@@ -48,6 +55,13 @@ ActiveRecord::Schema.define(:version => 20120407142824) do
     t.string   "location"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "auth_hash"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,13 @@
 # loaded once.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
 RSpec.configure do |config|
+  config.mock_with :rspec
+
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
