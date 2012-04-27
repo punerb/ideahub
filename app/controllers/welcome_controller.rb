5d99b2c06ecfae296b23b24bcab0a9f9d5ff6a2e
Anuja <anuja@joshsoftware.com>
2012-04-27 12:40:35 +0530

    Conflict resolved

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-<<<<<<< HEAD
-    @idea= Idea.first
-=======
     @ideas = Idea.limit(4).includes(:categories)
->>>>>>> e21cc7e9e1b41ef5d0771ac939f918075771a133
   end
 
 end
