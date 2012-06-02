c509878053ef830a7169bbaea526beb039163455
Gautam Rege <gautam@joshsoftware.com>
2012-06-02 16:15:52 +0530

    Styling participants

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -17,6 +17,6 @@ module IdeasHelper
     idea.users.collect{ |u|
       img_tag = image_tag("https://api.twitter.com/1/users/profile_image?screen_name=#{u.screen_name}&size=mini")
       link_to(img_tag, "http://twitter.com/#{u.screen_name}", :target => :_blank)
-    }.join(', ').html_safe
+    }.join.html_safe
   end
 end
