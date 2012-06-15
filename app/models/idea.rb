b40094abc2cf94f2bac5e852927f1cfe3d87303c
rtdp <ratnadeepdeshmane@gmail.com>
2012-06-15 19:04:47 +0530

    attr-accessible changes for idea model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -8,8 +8,8 @@ class Idea < ActiveRecord::Base
   has_many :categories, :through => :idea_categories
 
   validates :user_id, :title, :original_desc, :presence => true
-  
-  attr_accessible :title, :original_desc, :user_id, :category_ids, :github
+
+  attr_accessible :title, :original_desc, :user_id, :category_ids, :github, :description
 
   before_save :format_description
 
