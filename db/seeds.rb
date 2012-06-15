aa2b80a7ceda370fdb3429ce852b40236efc6c0c
rtdp <ratnadeepdeshmane@gmail.com>
2012-06-15 19:03:07 +0530

    seed improved for adding 5 users n 10 ideas randomly

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,24 @@ Category.create(
   %w{Ruby Javascript Rails CMS CRM Testing Nodejs Gem Api }.collect { |c| { name: c } }
 )
 
-Setting.consumer_key = 'your twitter consumer key'
-Setting.consumer_secret = 'your twitter consumer secret'
-Setting.oauth_token = 'your twitter access token'
-Setting.oauth_token_secret = 'your twitter access token secret'
+Setting.consumer_key = 'your consumer key here'
+Setting.consumer_secret = 'your consumer secret here.'
+#Setting.oauth_token = 'your twitter access token'
+#Setting.oauth_token_secret = 'your twitter access token secret'
 
+
+5.times do |i|
+  User.create({name: "test#{i}", email: "test#{i}@testers.com", screen_name: "testers#{i}"})
+end
+
+10.times do |i|
+  idea = Idea.new({
+    title: "Idea title#{i}",
+    description: "a. A integer. Et? Est diam ridiculus mattis scelerisque, scelerisque cras, in ultricies? Ultricies sociis velit quis scelerisque sit scelerisque cum diam a odio adipiscing? Duis egestas et sociis. Nec sed porttitor, pulvinar eros integer a aenean sociis placerat? Elit aliquam. Quis quis cum sit in velit? Massa duis sociis mus porta? ",
+    github: 'http://github.com/something/#{i}',
+    original_desc: "a. A integer. Et? Est diam ridiculus mattis scelerisque, scelerisque cras, in ultricies? Ultricies sociis velit quis scelerisque sit scelerisque cum diam a odio adipiscing? Duis egestas et sociis. Nec sed porttitor, pulvinar eros integer a aenean sociis placerat? Elit aliquam. Quis quis cum sit in velit? Massa duis sociis mus porta? "
+  })
+  idea.user = User.all.shuffle.first
+  idea.save
+end
 
